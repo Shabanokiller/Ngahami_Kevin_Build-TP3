e13c245ae79824783c70ab0f23f038be682d352c
kevin <kngahami@gmail.com>
2021-05-09 15:34:31 -0400

    Mofication du menu options

---
+++ Ngahami_Kevin_TP03-Mini-jeu/Assets/Script/volume.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class volume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider volumeAmbiance;
+    public Slider volumeFusil;
+    public Slider volumeMort;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeAmbiance.onValueChanged.AddListener(volumeAmbiance_OnValueChanged);
+        volumeFusil.onValueChanged.AddListener(volumeFusil_OnValueChanged);
+        volumeMort.onValueChanged.AddListener(volumeMort_OnValueChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void volumeAmbiance_OnValueChanged(float value)
+    {
+        mixer.SetFloat("MusicAmbiance", Mathf.Log(value) * 20f);
+    }
+
+    void volumeFusil_OnValueChanged(float value)
+    {
+
+    }
+
+    void volumeMort_OnValueChanged(float value)
+    {
+
+    }
+}
